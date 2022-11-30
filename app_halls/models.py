61514dcf1e17cdead27836da4142055bcc94702d
Endang Ismaya <endang.ismaya@gmail.com>
2022-11-30 21:10:35 +0700

    update signup with auto login after signup

--- app_halls/models.py
+++ app_halls/models.py
@@ -1,8 +1,11 @@
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Hall(models.Model):
     title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Video(models.Model):
--- app_halls/views.py
+++ app_halls/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from .models import Hall
+
 
 # Create your views here.
 def home(request):
@@ -16,6 +18,15 @@ class SignUp(generic.CreateView):
     success_url = reverse_lazy("home")
     template_name = "registration/signup.html"
 
+    def form_valid(self, form):
+        view_form = super(SignUp, self).form_valid(form)
+        username, password = form.cleaned_data.get("username"), form.cleaned_data.get(
+            "password1"
+        )
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return view_form
+
     # def get_context_data(self, **kwargs):
     #     context = super(SignUp, self).get_context_data(**kwargs)
     #     context.update({"user": {"is_authenticated": True}})
@@ -27,3 +38,8 @@ class CreateHall(generic.CreateView):
     fields = ["title"]
     template_name = "app_halls/create_hall.html"
     success_url = reverse_lazy("home")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        super(CreateHall, self).form_valid(form)
+        return redirect(reverse_lazy("home"))
